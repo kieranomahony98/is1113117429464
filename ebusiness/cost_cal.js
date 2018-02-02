627d2c31c8335eeb1c7b8f5540c4ac48ae828f87
Kieran O Mahony <117429464@umail.ucc.ie>
2018-02-02 11:24:53 +0000

    calculate cost

---
+++ ebusiness/cost_cal.js
@@ -0,0 +1,25 @@
+/* global $ */
+function calcSub(){
+    var argSubTotal;
+    if(document.getElementById('salesforce').checked){
+        argSubTotal = 100;
+    }
+    else {
+        argSubTotal = 300;
+    }
+    display(argSubTotal);
+}
+
+function display(parm1){
+    document.getElementById("SubTotal").value = parm1;
+    document.getElementById("Total").value = parm1;
+    
+    enablebtnProceed();
+}
+function enablebtnProceed(){
+    $('#btnProceed').prop('disabled', false);
+}
+
+function disablebtnProceed(){
+    $('#btnProceed').prop('disabled', true);
+}
