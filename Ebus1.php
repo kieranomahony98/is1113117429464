3d5a0351a40138300ef0c508d260a9c80304b260
Kieran O Mahony <117429464@umail.ucc.ie>
2018-02-09 11:05:27 +0000

    ebus

--- Ebus1.php
+++ Ebus1.php
@@ -4,9 +4,40 @@
         <Title>Select Product</Title>
         <!--jQuery-->
         <script  src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script type="text/javascript" src = "cost_calc.js"></script>
+        <script type="text/javascript" src="cost_calc.js"></script>        
+        <link rel="stylesheet" href="mystylesheet.css" type="text/css"/>
+
     </head>
     <body>
+        <h1 class="heading">eBusiness</h1>
+             <div class="back">
+            <ul>
+              <li><a href="homepage.html">Homepage</a></li>
+              <li class="dropdown">
+                  <a href="javascript:void(0)" class="dropbtn">Interests</a>
+                  <div class="dropdown-content">
+                  <a href="travel.html">Travel</a>
+                  <a href="sport.html">Sport</a>
+                  </div>
+              </li>
+              <li class="dropdown">
+                  <a href="javascript:void(0)" class="dropbtn">CV</a>
+                  <div class="dropdown-content">
+                  <a href="cv_page1.html">Personal Details</a>
+                  <a href="cv_page2.html">Education</a>
+                  <a href="cv_page3.html">Work Experience</a>
+                  </div>
+              </li>
+                                <li class="dropdown">
+                  <a href="javascript:void(0)" class="dropbtn">eBusiness</a>
+                  <div class="dropdown-content">
+                  <a href="Ebus1.php">Buisness 1</a>
+                  <a href="Ebus2.php">Buisness 2</a>
+                  </div>
+              </li>
+            </ul>
+        </div>
+            <br/><br/>
         <h4>Select a Product</h4>
         <br/>
         <form method = "POST" action = "ebus2.php">
--- Ebus2.php
+++ Ebus2.php
@@ -1,14 +1,19 @@
+
 <!DOCTYPE html>
 <html>
     <head>
-        <title> IS113 Project</title>
-        <link rel="stylesheet" href="mystylesheet.css" type="text/css"/>
-    
-    </head>
-    <body>
-        <h1 class="heading">117429464:IS113 Project</h1>
         
-        <div class="back">
+        <title> Enter Details</title>
+        
+        
+        <!--jQuery-->
+        <link rel="stylesheet" href="mystylesheet.css" type="text/css"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    
+    <body>
+        <h1 class="heading">eBusiness</h1>
+         <div class="back">
         <br/><br/>
             <ul>
               <li><a href="homepage.html">Homepage</a></li>
@@ -26,18 +31,47 @@
                   <a href="cv_page2.html">Education</a>
                   <a href="cv_page3.html">Work Experience</a>
                   </div>
-                  </li>
-                  <li class="dropdown">
+              </li>
+                <li class="dropdown">
                   <a href="javascript:void(0)" class="dropbtn">eBusiness</a>
                   <div class="dropdown-content">
                   <a href="Ebus1.php">Buisness 1</a>
                   <a href="Ebus2.php">Buisness 2</a>
                   </div>
               </li>
-                </ul>
-                </div>
-                 
-        <br/><br/>
-        <marquee behavior="scroll" direction="right">Welcome!</marquee>
+            </ul>
+        </div>
+            <br/><br/>
+        
+        <h4>Please enter your payment details.</h4>
+        
+            <br />
+            
+            <form method = "POST" action = "Ebus3.php">
+                
+                <label for="user_pin">
+                     PIN 
+                </label>
+                
+                <input type="password" id="user_pin" placeholder="Card Pin" maxlength="4">
+                    
+                <button type="Submit" id="btnPurchase" disabled> 
+                    Proceed with Purchase 
+                </button>
+                
+            </form>
+            
+            <br />
+            
+            <button onClick="validateDetails()"> Validate </button>
+        
+        <script type="text/javascript" src="ebus2_validator.js"></script>
+        
+        
     </body>
+    
+    
+    
 </html>
+
+
